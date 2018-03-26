86d91e5a6bf45fa676fb9ce3dd008912d42e4ade
play4fun <play4fun@foxmail.com>
2018-03-26 14:42:55 +0800

    视频

--- 网站开发/Django/tutorial/helloworld/views.py
+++ 网站开发/Django/tutorial/helloworld/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the helloworld index.")
-    # return render(request,template_name='helloworld/index.html')
+    # return HttpResponse("Hello, world. You're at the helloworld index.")
+    return render(request,template_name='helloworld/index.html')
--- 网站开发/Django/tutorial/tutorial/settings.py
+++ 网站开发/Django/tutorial/tutorial/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #
-    # 'helloworld.apps.HelloworldConfig',
+    'helloworld.apps.HelloworldConfig',
 ]
 
 MIDDLEWARE = [
