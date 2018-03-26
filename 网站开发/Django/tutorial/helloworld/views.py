5194e2c16d3fae44d5c8cab5bbc06886beed55c3
play4fun <play4fun@foxmail.com>
2018-03-26 14:30:26 +0800

    模板Template

--- 网站开发/Django/tutorial/helloworld/views.py
+++ 网站开发/Django/tutorial/helloworld/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the helloworld index.")
+    # return render(request,template_name='helloworld/index.html')
