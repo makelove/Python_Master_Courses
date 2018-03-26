5c0f8e1510854c4fa2fa7dbbd2a321e4d5b1cb1e
play4fun <play4fun@foxmail.com>
2018-03-26 15:02:28 +0800

    视频

--- 网站开发/Django/tutorial/helloworld/views.py
+++ 网站开发/Django/tutorial/helloworld/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the helloworld index.")
-    return render(request,template_name='helloworld/index.html')
+    context = {'title': '欢迎光临首页', 'name': 'xiaoMing', 'age': 14, 'class': '高一3班', 'scores': [73, 84, 68, 93]}
+    return render(request,template_name='helloworld/index.html',context=context)
