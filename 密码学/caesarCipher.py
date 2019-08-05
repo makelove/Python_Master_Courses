d3c92a587728f55f0763dd3fcbe2dac9c023cc90
play4fun <play4fun@foxmail.com>
2019-08-05 11:19:42 +0800

    fix

--- 密码学/caesarCipher.py
+++ 密码学/caesarCipher.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
 '''
 凯撒加密法
+
+只针对 字母 编码
+数字不行
 '''
 
-def code(s,k):
-	rt=[]
-	for c in s.upper():
-		if c in string.ascii_uppercase:
-			n=ord(c)+k
-			if n>90:
-				n=n-26
-			c=chr(n)
-		rt.append(c)
-	return ''.join(rt)
+import string
 
-def decode(s,k):
-	rt=[]
-	for c in s.upper():
-		if c in string.ascii_uppercase:
-			n=ord(c)-k
-			if n<65:
-				n=n+26
-			c=chr(n)
-		rt.append(c)
-	return ''.join(rt)
+
+def encode(s, key='H'):
+    rt = []
+    for c in s.upper():  # 大写
+        if c in string.ascii_uppercase:
+            n = ord(c) + ord(key)
+            if n > 90:
+                n = n - 26
+            c = chr(n)
+        rt.append(c)
+    return ''.join(rt)
+
+
+def decode(s, key='H'):
+    rt = []
+    for c in s.upper():
+        if c in string.ascii_uppercase:
+            n = ord(c) - ord(key)
+            if n < 65:
+                n = n + 26
+            c = chr(n)
+        rt.append(c)
+    return ''.join(rt)
